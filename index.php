089d03dea4df24921a049a8dd66705af8c881370
NR DEV <nicolas.ribeiro@academico.ifpb.edu.br>
2023-11-20 23:46:29 -0300

    atualização

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
     <form action="cadastrar.php" method="post">
         <label for="name">Nome:</label><input type="text" name="nome_paciente">
         <label for="ema">Email:</label><input type="email" name="email_paciente">
-        <label for="num">Numero:</label><input type="text" name="num_paciente" placeholder="(XX) XXXXX-XXXX" oninput="formatarTelefone(this)>
-        <label for=" dt">Dt de nascimento:</label><input type="date" name="dt_paciente">
+        <label for="num">Numero:</label><input type="text" name="num_paciente" placeholder="(XX) XXXXX-XXXX" oninput="formatarTelefone(this)">
+        <label for="dt">Dt de nascimento:</label><input type="date" name="dt_paciente">
         <input type="submit" value="Enviar para o BD">
     </form>
 
